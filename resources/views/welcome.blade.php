155207c3f96b6bc5cf21ef6a5106016b49bdc58e
Roberto Ricco <riccoroberto07@gmail.com>
2025-08-01 04:55:37 +0200

    ProgettoWood iniziato, scaffolding fatto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Titolo</h1>
+            </div>
+        </div>
+    </div>
+</x-layout>
