87b4d32c1cfa99206c153ed131b4a1f1f20babd8
Roberto Ricco <riccoroberto07@gmail.com>
2025-08-09 15:34:35 +0200

    Buttone logout integrato, customizzato form e button register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,27 +1,27 @@
 <x-layout>
     <section class="container my-5">
-        <h1 class="text-center display-6">Registrati</h1>
+        <h1 class="text-center display-6 m-5">Registrati</h1>
         <div class="row">
             <div class="col-12">
-                <form class="rounded shadow p-5" method="POST" action="{{ route('register') }}">
+                <form class="formRegister p-5" method="POST" action="{{ route('register') }}">
                     @csrf
                     <div class="mb-3">
                         <label for="userName" class="form-label">Inserisci Nome</label>
-                        <input type="text" class="form-control" id="userName" aria-describedby="emailHelp" name="name">
+                        <input type="text" class="form-control" id="userName" name="name">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Inserisci Email</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+                        <label for="userEmail" class="form-label">Inserisci Email</label>
+                        <input type="email" class="form-control" id="userEmail" aria-describedby="emailHelp" name="email">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Inserisci Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+                        <label for="inputPassword" class="form-label">Inserisci Password</label>
+                        <input type="password" class="form-control" id="inputPassword" name="password">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPasswordConfirmation" class="form-label">onferma Password</label>
-                        <input type="password" class="form-control" id="exampleInputPasswordConfirmation" name="password_confirmation">
+                        <label for="inputPasswordConfirmation" class="form-label">Conferma Password</label>
+                        <input type="password" class="form-control" id="inputPasswordConfirmation" name="password_confirmation">
                     </div>
-                    <button type="submit" class="btn btn-primary">Registrati</button>
+                    <button type="submit" class="btnRgistrati">Registrati</button>
                 </form>
             </div>
         </div>
